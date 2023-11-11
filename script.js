8f6f229f3f6b0657dd41f08dc191a214279aecb4
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-11 22:09:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function fibonacci(num) {
 // your code here
 	let num1=0;
 	let num2=1;
-	let sum;
+	let sum=0;
 	let i=0;
-	for( i=0;i<num;i++)
+	for( i=0; i<num; i++)
 		{
 			sum=num1+num2;
 			num1=num2;
